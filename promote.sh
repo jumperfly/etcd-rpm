7f8212b540faafbd0c1b41733d50350a5e452e55
jumperfly <git@jumperfly.co.uk>
2018-11-19 21:06:16 +0000

    Fixed names in promote script

--- promote.sh
+++ promote.sh
@@ -5,9 +5,14 @@ export RPM_RELEASE=${fullver[1]}
 export RPM_VERSION=${fullver[0]}
 export YUM_PATH=stable
 
+RPM_NAME=etcd-$RPM_VERSION-$RPM_RELEASE.el7.x86_64.rpm
+RPM_URL=https://dl.bintray.com/jumperfly/yum/etcd/latest/$RPM_NAME
+
 echo "########## Starting promote for version $RPM_VERSION-$RPM_RELEASE ##########"
 mkdir -p build/RPMS/x86_64
 cd build/RPMS/x86_64
-curl -LO https://dl.bintray.com/jumperfly/yum/cfssl/latest/cfssl-$RPM_VERSION-$RPM_RELEASE.el7.x86_64.rpm
+echo Downloading $RPM_URL
+curl -fLO $RPM_URL || exit 1
+echo Downloaded $(md5sum $RPM_NAME)
 cd ../../..
 envsubst < bintray-descriptor.json > build/bintray-descriptor.json
